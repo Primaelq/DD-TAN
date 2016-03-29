5857f3b3943f483860ecdf8698a6b61a7e850bc8
Primael <primaelq@gmail.com>
2016-03-29 18:53:48 +0200

    new commit

---
+++ AndroidStudio/app/src/main/java/com/apps/primael/dd_tan/CustomView.java
@@ -0,0 +1,23 @@
+package com.apps.primael.dd_tan;
+
+import android.content.Context;
+import android.graphics.Paint;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class CustomView extends SurfaceView implements Runnable
+{
+
+    SurfaceHolder holder;
+    Paint paint;
+
+    public CustomView(Context context)
+    {
+        super(context);
+    }
+
+    public void run()
+    {
+
+    }
+}
---
+++ AndroidStudio/app/src/main/java/com/apps/primael/dd_tan/Display.java
@@ -0,0 +1,16 @@
+package com.apps.primael.dd_tan;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Display extends Activity
+{
+    CustomView customView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+    }
+}
